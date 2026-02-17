ec6ca8baf2f12fb7ab60dae8aee4aefacedef2c9
zitajoubert <zita.joubert@spatialedge.ai>
2026-02-17 13:35:16 +0200

    proxy api

---
+++ fastapi_proxy/main.py
@@ -0,0 +1,9 @@
+import requests
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+
+RequestsInstrumentor().instrument()
+
+@app.get("/caller")
+def caller():
+    response = requests.get("http://localhost:8000/rolldice")
+    return response.json()

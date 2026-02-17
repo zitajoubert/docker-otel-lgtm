e9ae02206a339570e513b79cafec3dbb247c8077
zitajoubert <zita.joubert@spatialedge.ai>
2026-02-17 13:41:58 +0200

    print statement

--- fastapi_proxy/main.py
+++ fastapi_proxy/main.py
@@ -6,4 +6,5 @@ RequestsInstrumentor().instrument()
 @app.get("/caller")
 def caller():
     response = requests.get("http://localhost:8000/rolldice")
+    print("Connected to rolldice api")
     return response.json()

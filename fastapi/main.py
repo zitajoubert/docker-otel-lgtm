e3663978510d22b9ae95f35fab4c05b845e2df48
zitajoubert <zita.joubert@spatialedge.ai>
2026-02-19 09:53:21 +0200

    ensure fastapi running

--- fastapi/main.py
+++ fastapi/main.py
@@ -56,3 +56,7 @@ def roll_dice(roll: Optional[int] = None):
     finally:
         if conn:
             conn.close()
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)

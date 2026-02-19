afc761f98b960ad307c46e8b0025b92d10c98d3c
zitajoubert <zita.joubert@spatialedge.ai>
2026-02-19 11:03:02 +0200

    port mismatch

--- fastapi_proxy/main.py
+++ fastapi_proxy/main.py
@@ -21,5 +21,5 @@ def caller():
         return {"error": str(e)}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8001, log_config=None)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, log_config=None)
 

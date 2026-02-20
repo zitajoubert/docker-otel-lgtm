78db77d2d376909bc7c874825a5835ef81f39447
zitajoubert <zita.joubert@spatialedge.ai>
2026-02-20 10:47:44 +0200

    remove code

--- fastapi/main.py
+++ fastapi/main.py
@@ -57,6 +57,3 @@ def roll_dice(roll: Optional[int] = None):
         if conn:
             conn.close()
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8001)
